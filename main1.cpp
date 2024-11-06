a9f816856dfc11cad002329e62684ff5deb3b073
SashaKhaytina <sasha-khaytina@rambler.ru>
2024-11-06 23:02:03 +0300

    some fixes

--- main1.cpp
+++ main1.cpp
@@ -3,51 +3,57 @@
 #include <string.h>
 #include <ctype.h>
 
-#define TEST1    list_append(&st_list, 111, 1);\
-    print_arr(st_list.list, LEN_LIST + 1);\
-    print_arr(st_list.next, LEN_LIST + 1);\
-    print_arr(st_list.prev, LEN_LIST + 1);\
-    print_real_list(&st_list);\
-    dump(&st_list, &dumps_counter);\
+#define TEST1    list_append(&st_list, 111, 1, &st_dump);\
+    dump_normal_person(&st_list);\
 \
-    list_append(&st_list, 222, 2);\
-    print_arr(st_list.list, LEN_LIST + 1);\
-    print_arr(st_list.next, LEN_LIST + 1);\
-    print_arr(st_list.prev, LEN_LIST + 1);\
-    print_real_list(&st_list);\
-    dump(&st_list, &dumps_counter);\
+    list_append(&st_list, 222, 2, &st_dump);\
+    dump_normal_person(&st_list);\
 \
-    list_pop(&st_list, 2);\
-    print_arr(st_list.list, LEN_LIST + 1);\
-    print_arr(st_list.next, LEN_LIST + 1);\
-    print_arr(st_list.prev, LEN_LIST + 1);\
-    print_real_list(&st_list);\
-    dump(&st_list, &dumps_counter);
+    list_pop(&st_list, 2, &st_dump);\
+    dump_normal_person(&st_list);
 
-#define TEST2     list_append(&st_list, 111, 1);\
-    print_arr(st_list.list, LEN_LIST + 1);\
-    print_arr(st_list.next, LEN_LIST + 1);\
-    print_arr(st_list.prev, LEN_LIST + 1);\
-    print_real_list(&st_list);\
-    dump(&st_list, &dumps_counter);\
+
+#define TEST2     list_append(&st_list, 111, 1, &st_dump);\
+    dump_normal_person(&st_list);\
 \
-    list_append(&st_list, 222, 1);\
-    print_arr(st_list.list, LEN_LIST + 1);\
-    print_arr(st_list.next, LEN_LIST + 1);\
-    print_arr(st_list.prev, LEN_LIST + 1);\
-    print_real_list(&st_list);\
-    dump(&st_list, &dumps_counter);\
+    list_append(&st_list, 222, 1, &st_dump);\
+    dump_normal_person(&st_list);\
 \
-    list_pop(&st_list, 1);\
-    print_arr(st_list.list, LEN_LIST + 1);\
-    print_arr(st_list.next, LEN_LIST + 1);\
-    print_arr(st_list.prev, LEN_LIST + 1);\
-    print_real_list(&st_list);\
-    dump(&st_list, &dumps_counter);
+    list_pop(&st_list, 1, &st_dump);\
+    dump_normal_person(&st_list);
+
+
+#define TEST3    list_append(&st_list, 111, 1, &st_dump);\
+    dump_normal_person(&st_list);\
+\
+    list_append(&st_list, 222, 2, &st_dump);\
+    dump_normal_person(&st_list);\
+\
+    list_pop(&st_list, 1, &st_dump);\
+    dump_normal_person(&st_list);\
+\
+    list_append(&st_list, 223, 2, &st_dump);\
+    dump_normal_person(&st_list);\
+\
+    list_append(&st_list, 224, 3, &st_dump);\
+    dump_normal_person(&st_list);
+
 
 
 const size_t LEN_LIST = 10;
 const char* const DUMP_FILE = "pictures/log.html"; // почему log
+const char* const FONT_COLOR = "#191970";
+const char* const ELEM_LIST_COLOR = "#AFEEEE";
+const char* const NEXT_EDGE_COLOR = "#7FFF00";
+const char* const PREV_EDGE_COLOR = "#DC143C";
+const char* const HEAD_AND_TAIL_COLOR = "#FF8C00";
+
+enum Errors
+{
+    OK, 
+    ERROR_OPEN_FILE,
+    ERROR_INDEX
+};
 
 
 struct List
@@ -63,14 +69,19 @@ struct List
     int tail; // наиболее раншьий добавленный (откуда pop) для очереди. По сути начало списка...  
 
 };
- 
+
+struct ForDump
+{
+    char commands[100][30];
+    int dumps_counter;
+};
 
 
 // можно возвращать коды ошибок
 
 void init_list  (List* st_list);
-void list_append(List* st_list, int elem, int ind); // ind - на какой индекс ХОТИМ ПОСТАВИТЬ
-void list_pop   (List* st_list, int ind);           // ind - с какого индекса ХОТИМ УДАЛИТЬ              
+void list_append(List* st_list, int elem, int ind, ForDump* st_dump); // ind - на какой индекс ХОТИМ ПОСТАВИТЬ
+void list_pop   (List* st_list, int ind, ForDump* st_dump);           // ind - с какого индекса ХОТИМ УДАЛИТЬ              
 
 int find_empty_ind(List* st_list);
 int who_is_num_ind_now(List* st_list, int ind);
@@ -78,15 +89,11 @@ int who_is_num_ind_now(List* st_list, int ind);
 void print_arr(int* arr, size_t size);
 void print_real_list(List* st_list);
 
-void dump(List* st_list, int* dumps_counter);
+void dump(List* st_list, ForDump* st_dump);
 void create_png(int num);
-void to_do_log_file(int num); // num - их количесвто
+void to_do_log_file(ForDump* st_dump); // num - их количесвто
 
-
-
-
-// Поставить проверку на корректность индекса (то что не превысили ли size своим запросом)
-
+void dump_normal_person(List* st_list);
 
 
 
@@ -95,78 +102,22 @@ int main()
     List st_list;
     init_list(&st_list);
 
-    int dumps_counter = 0;
+    ForDump st_dump = {};
 
+    TEST3
 
-    // list_append(&st_list, 111, 1);
-    // print_arr(st_list.list, LEN_LIST + 1);
-    // print_arr(st_list.next, LEN_LIST + 1);
-    // print_arr(st_list.prev, LEN_LIST + 1);
-    // print_real_list(&st_list);
-
-    // list_append(&st_list, 222, 2);
-    // print_arr(st_list.list, LEN_LIST + 1);
-    // print_arr(st_list.next, LEN_LIST + 1);
-    // print_arr(st_list.prev, LEN_LIST + 1);
-    // print_real_list(&st_list);
-    // dump(&st_list, &dumps_counter);
-
-    // list_pop(&st_list, 1);
-    // print_arr(st_list.list, LEN_LIST + 1);
-    // print_arr(st_list.next, LEN_LIST + 1);
-    // print_arr(st_list.prev, LEN_LIST + 1);
-    // print_real_list(&st_list);
-    // dump(&st_list, &dumps_counter);
-
-    // list_append(&st_list, 223, 2);
-    // print_arr(st_list.list, LEN_LIST + 1);
-    // print_arr(st_list.next, LEN_LIST + 1);
-    // print_arr(st_list.prev, LEN_LIST + 1);
-    // print_real_list(&st_list);
-    // dump(&st_list, &dumps_counter);
-    
-    
-    // list_append(&st_list, 224, 3);
-    // print_arr(st_list.list, LEN_LIST + 1);
-    // print_arr(st_list.next, LEN_LIST + 1);
-    // print_arr(st_list.prev, LEN_LIST + 1);
-    // print_real_list(&st_list);
-    // dump(&st_list, &dumps_counter);
-
-
-    TEST2
+    // TEST2
 
     // TEST1
 
-    // list_append(&st_list, 221, 1);
-
-    // list_append(&st_list, 222, 2);
-
-    // printf("%d - size\n", st_list.size);
-    // print_arr(st_list.list, LEN_LIST + 1);
-    // print_arr(st_list.next, LEN_LIST + 1);
-    // print_arr(st_list.prev, LEN_LIST + 1);
-    // print_real_list(&st_list);
-
-    // list_pop(&st_list, 1);
-    // print_arr(st_list.list, LEN_LIST + 1);
-    // print_arr(st_list.next, LEN_LIST + 1);
-    // print_arr(st_list.prev, LEN_LIST + 1);
-    // print_real_list(&st_list);
-
-    // list_append(&st_list, 223, 3);
-    // print_arr(st_list.list, LEN_LIST + 1);
-    // print_arr(st_list.next, LEN_LIST + 1);
-    // print_arr(st_list.prev, LEN_LIST + 1);
-    // print_real_list(&st_list);
-    to_do_log_file(dumps_counter);
+    to_do_log_file(&st_dump);
 
 }
 
 
 void init_list(List* st_list)
 {
-    st_list->list[0] = 0; // ничего не лежит
+    st_list->list[0] = 0; // фантомный элемент
     st_list->next[0] = 0;
     st_list->prev[0] = 0;
 
@@ -181,27 +132,15 @@ void init_list(List* st_list)
     st_list->head = 0;
     st_list->tail = 0;
 
-    // st_list->list[1] = 111;
-    // st_list->list[2] = 222;
-    // st_list->prev[1] = 0;
-    // st_list->next[1] = 2;
-    // st_list->prev[2] = 1;
-    // st_list->next[2] = 0;
-
-
-    // st_list->size = 2;
-    // st_list->head = 2;
-    // st_list->tail = 1;
-
 }
 
 
-void list_append(List* st_list, int elem, int ind)
+void list_append(List* st_list, int elem, int ind, ForDump* st_dump)
 {
     // В list оно добавляется просто на первый свободный элемент!
     // Т.е надо найти свободный индекс и добавить в него. НО ЭТО НЕ ФАКТ, ЧТО БУДЕТ ЕГО МЕСТОМ В САМОМ СПИСКЕ
     int empty_ind = find_empty_ind(st_list); // индекс, на котором он реально лежит в списке. 
-    printf("%d - ei\n", empty_ind);
+    // printf("%d - ei\n", empty_ind);
 
     if (empty_ind == -1)
     {
@@ -215,6 +154,12 @@ void list_append(List* st_list, int elem, int ind)
         return;
     }
 
+    char new_str[30] = {};
+    sprintf(new_str, "append to ind %d elem %d", ind, elem);
+
+    strcpy(st_dump->commands[st_dump->dumps_counter + 1], new_str);
+    // printf("%s - st_dump->commands[st_dump->dumps_counter + 1]\n", st_dump->commands[st_dump->dumps_counter + 1]);
+
     // Надо найти, кто СЕЙЧАС (до добавления) под номером ind. Для этого надо сделать ind шагов от TAIL
 
 
@@ -227,38 +172,38 @@ void list_append(List* st_list, int elem, int ind)
 
     // }
 
-    // else
-    // {
-        printf("st_list->size != 0\n");
-        int was_ind = who_is_num_ind_now(st_list, ind);
-        printf("%d - was_ind\n", was_ind);
-        st_list->list[empty_ind] = elem;
-        st_list->next[st_list->prev[was_ind]] = empty_ind;
-        st_list->next[empty_ind] = was_ind;
+    int was_ind = who_is_num_ind_now(st_list, ind);
+    // printf("%d - was_ind\n", was_ind);
+    st_list->list[empty_ind] = elem;
+    st_list->next[st_list->prev[was_ind]] = empty_ind;
+    st_list->next[empty_ind] = was_ind;
 
-        st_list->prev[empty_ind] = st_list->prev[was_ind];
-        st_list->prev[was_ind] = empty_ind;
-    // }
+    st_list->prev[empty_ind] = st_list->prev[was_ind];
+    st_list->prev[was_ind] = empty_ind;
 
     st_list->size++;
 
-
-
-
-
+    dump(st_list, st_dump);
 
 
 }
 
 
 
-void list_pop   (List* st_list, int ind)
+void list_pop   (List* st_list, int ind, ForDump* st_dump)
 {
     if (st_list->size == 0)
     {
         printf("Попытка удаления из пустого списка\n");
         return;
     }
+
+    char new_str[30] = {};
+    sprintf(new_str, "pop elem on ind %d", ind);
+    //st_dump->commands[st_dump->dumps_counter + 1] = new_str2;
+    strcpy(st_dump->commands[st_dump->dumps_counter + 1], new_str);
+
+
 
     int was_ind = who_is_num_ind_now(st_list, ind);
     printf("%d - was_ind\n", was_ind);
@@ -271,6 +216,8 @@ void list_pop   (List* st_list, int ind)
     st_list->list[was_ind] = -1;
     st_list->next[was_ind] = -1;
     st_list->prev[was_ind] = -1;
+
+    dump(st_list, st_dump);
 
 }
 
@@ -313,7 +260,7 @@ void print_arr(int* arr, size_t size)
 }
 
 
-void print_real_list(List* st_list) // тут можно юолее быстро. Просто чтоб после каждого шага печатала.
+void print_real_list(List* st_list) // тут можно быстрее. Просто чтоб после каждого шага печатала.
 {
     for (size_t i = 1; i <= st_list->size; i++)
     {
@@ -332,25 +279,29 @@ void print_real_list(List* st_list) // тут можно юолее быстро
 
 
 
-void dump(List* st_list, int* dumps_counter)
+void dump(List* st_list, ForDump* st_dump)
 {
-    (*dumps_counter)++;
-    int number_of_dump = *dumps_counter;
+
+    st_dump->dumps_counter++;
+    int number_of_dump = st_dump->dumps_counter;
+
+    // printf("%d - st_dump->dumps_counter\n", st_dump->dumps_counter);
 
 
-    char sample[] = {'p', 'i', 'c', 't', 'u', 'r', 'e', 's', '/', 'i', 'm', 'a', 'g', 'e', '0', '0', '.', 'd', 'o', 't'};
+    char sample[21] = {'p', 'i', 'c', 't', 'u', 'r', 'e', 's', '/', 'i', 'm', 'a', 'g', 'e', '0', '0', '.', 'd', 'o', 't'};
     sample[14] = (char) ('0' + ((int) number_of_dump / 10));
     sample[15] = (char) ('0' + ((int) number_of_dump % 10));
+    // printf("%s - sample\n", sample);
     FILE* file = fopen(sample, "w");
 
-    fprintf(file, "digraph\n{\nbgcolor=\"#191970\";\nrankdir = LR;\nedge[style=\"invis\", weight = 1000000];\n");
+    fprintf(file, "digraph\n{\nbgcolor=\"%s\";\nrankdir = LR;\nedge[style=\"invis\", weight = 1000000];\n", FONT_COLOR);
 
     //IND_1[shape=record, label = "IND = 1 | value = | next = | prev = ", style="filled",fillcolor="#AFEEEE"];
 
 
     for (int i = 0; i < LEN_LIST + 1; i++) // создали все ячейки
     {
-        fprintf(file, "IND_%d[shape=Mrecord, label = \"IND = %d | value = %d | next = %d | prev = %d \", style=\"filled\",fillcolor=\"#AFEEEE\"]\n", i, i, st_list->list[i], st_list->next[i], st_list->prev[i]);
+        fprintf(file, "IND_%d[shape=Mrecord, label = \"IND = %d | value = %d | next = %d | prev = %d \", style=\"filled\",fillcolor=\"%s\"]\n", i, i, st_list->list[i], st_list->next[i], st_list->prev[i], ELEM_LIST_COLOR);
     }
 
     for (int i = 0; i < LEN_LIST; i++) // создали все ячейки
@@ -358,19 +309,17 @@ void dump(List* st_list, int* dumps_counter)
         fprintf(file, "IND_%d -> IND_%d\n", i, i + 1);
     }
 
-    // e width and height at
-
-    fprintf(file, "TAIL[shape=\"rectangle\", width = 0.5, height = 0.4, style=\"filled\", fillcolor=\"#FF8C00\"];\n");
-    fprintf(file, "HEAD[shape=\"rarrow\", width = 0.5, height = 0.5, style=\"filled\", fillcolor=\"#FF8C00\"];\n");
+    fprintf(file, "TAIL[shape=\"rectangle\", width = 0.5, height = 0.4, style=\"filled\", fillcolor=\"%s\"];\n", HEAD_AND_TAIL_COLOR);
+    fprintf(file, "HEAD[shape=\"rarrow\", width = 0.5, height = 0.5, style=\"filled\", fillcolor=\"%s\"];\n", HEAD_AND_TAIL_COLOR);
     fprintf(file, "{ rank = same; TAIL; IND_0}\n");
     fprintf(file, "TAIL -> HEAD\n");
 
 
 
     // Сделаем связи NEXT зелеными стрелками
-    fprintf(file, "edge[color=\"#7FFF00\", weight = 1, style=\"\"];\n");
+    fprintf(file, "edge[color=\"%s\", weight = 1, style=\"\"];\n", NEXT_EDGE_COLOR);
 
-    for (int i = 0; i < LEN_LIST + 1; i++)
+    for (int i = 1; i < LEN_LIST + 1; i++) // если запускать с 0, то на рисунке трешак, который только путает
     {
         if (st_list->next[i] == -1) continue;
         fprintf(file, "IND_%d -> IND_%d;\n", i, st_list->next[i]);
@@ -379,9 +328,9 @@ void dump(List* st_list, int* dumps_counter)
 
 
     // Сделаем связи PREV красными стрелками
-    fprintf(file, "edge[color=\"#DC143C\", weight = 1, style=\"\"];\n");
+    fprintf(file, "edge[color=\"%s\", weight = 1, style=\"\"];\n", PREV_EDGE_COLOR);
 
-    for (int i = 0; i < LEN_LIST + 1; i++)
+    for (int i = 1; i < LEN_LIST + 1; i++) // если запускать с 0, то на рисунке трешак, который только путает
     {
         if (st_list->prev[i] == -1) continue; // это возможно?
         fprintf(file, "IND_%d -> IND_%d;\n", i, st_list->prev[i]);
@@ -394,12 +343,11 @@ void dump(List* st_list, int* dumps_counter)
 
     create_png(number_of_dump);
 
-
 }
 
 void create_png(int num)
 {
-    char command_create_png[100] = {};
+    char command_create_png[200] = {};
     sprintf(command_create_png, "dot pictures/image%d%d.dot -Tpng -o pictures/pic%d%d.png", num / 10, num % 10, num / 10, num % 10);
     system(command_create_png);
 }
@@ -407,14 +355,14 @@ void create_png(int num)
 
 
 
-void to_do_log_file(int num)
+void to_do_log_file(ForDump* st_dump)
 {
     FILE* file = fopen(DUMP_FILE, "w");
 
     fprintf(file, "<pre>\n");
-    fprintf(file, "<style>body {background-color:#191970}</style>\n\n");
+    fprintf(file, "<style>body {background-color:%s}</style>\n\n", FONT_COLOR);
 
-    for (int i = 1; i <= num; i++)
+    for (int i = 1; i <= st_dump->dumps_counter; i++)
     {
 
         //char sample[] = {'p', 'i', 'c', 't', 'u', 'r', 'e', 's', '/', 'p', 'i', 'c', '0', '0', '.', 'p', 'n', 'g'};
@@ -423,8 +371,9 @@ void to_do_log_file(int num)
         sample[3] = (char) ('0' + ((int) i / 10));
         sample[4] = (char) ('0' + ((int) i % 10));
 
+        // printf("%s - ssssssssssssssssssssss\n", st_dump->commands[i]);
 
-        fprintf(file, "<big><big><h style=\"color:#FF8C00\">LIST (print %d) &#128578;</h></big></big>\n\n", i);
+        fprintf(file, "<big><big><h style=\"color:#FF8C00\">LIST %s (print %d) &#128578;</h></big></big>\n\n", st_dump->commands[i], i); //  сюда строку!
 
 
         fprintf(file, "<img src=\"%s\">\n\n\n", sample);
@@ -432,4 +381,14 @@ void to_do_log_file(int num)
     }
 
     fclose(file);
+}
+
+
+
+void dump_normal_person(List* st_list)
+{
+    print_arr(st_list->list, LEN_LIST + 1);
+    print_arr(st_list->next, LEN_LIST + 1);
+    print_arr(st_list->prev, LEN_LIST + 1);
+    print_real_list(st_list);
 }
